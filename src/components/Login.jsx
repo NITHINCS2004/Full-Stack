e51d57899d421b94abe5faf524cb1c113df2716a
123456NI-GIT <157794860+123456NI-GIT@users.noreply.github.com>
2025-03-23 21:01:33 +0530

    message

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -27,7 +27,7 @@ const Login = ({ onLogin }) => {
 };
 
 export default Login;
-*//*
+*/
 import React from "react";
 import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
@@ -57,43 +57,3 @@ const Login = ({ setUser }) => {
 };
 
 export default Login;
-*/
-
-import React from "react";
-import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
-import { jwtDecode } from "jwt-decode";
-import { useNavigate } from "react-router-dom";
-
-
-import "../styles/Login.css";
-const Login = () => {
-  const navigate = useNavigate();
-
-  const handleSuccess = (credentialResponse) => {
-    const details = jwtDecode(credentialResponse.credential);
-    console.log(details);
-    localStorage.setItem("user", JSON.stringify(details));
-    navigate("/");
-  };
-
-  return (
-    <div className="login-container">
-      <h1 className="login-heading">WELCOME TO YOUTUBE</h1>
-      <div className="icon-container">
-        <BsFillShieldLockFill size={40} className="icon" />
-        <BsKeyFill size={40} className="icon" />
-      </div>
-      <div className="google-login-box">
-        <GoogleOAuthProvider clientId="486340617177-627cq1s06j912ps9ot0lnrqube6b4vlp.apps.googleusercontent.com">
-          <GoogleLogin onSuccess={handleSuccess} onError={() => console.log("Login Failed")} />
-        </GoogleOAuthProvider>
-      </div>
-      <p className="click-here-text">Click here to login</p>
-    </div>
-  );
-};
-
-export default Login;
-
-
-
