4db52b446f50d915ff2446aa49431a4209e4de33
123456NI-GIT <157794860+123456NI-GIT@users.noreply.github.com>
2025-03-23 21:09:11 +0530

    message

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -46,32 +46,20 @@ const Login = ({ setUser }) => {
     navigate("/home"); // Redirect to Home Page
   };
 
-  /*return (
-    <div className="login-container">
-      <h1 className="login-heading">WELCOME TO YOUTUBE</h1>
-      <div className="google-login-box">
-        <GoogleLogin onSuccess={handleSuccess} onError={() => console.log("Login Failed")} />
-      </div>
-      <p className="click-here-text">Click here to login</p>
-    </div>
-  );
-};*/
-return (
+  return (
     <div className="login-container">
       <h1 className="login-heading">WELCOME TO YOUTUBE</h1>
       <div className="icon-container">
         <BsFillShieldLockFill size={40} className="icon" />
         <BsKeyFill size={40} className="icon" />
       </div>
+
       <div className="google-login-box">
-        <GoogleOAuthProvider clientId="486340617177-627cq1s06j912ps9ot0lnrqube6b4vlp.apps.googleusercontent.com">
-          <GoogleLogin onSuccess={handleSuccess} onError={() => console.log("Login Failed")} />
-        </GoogleOAuthProvider>
+        <GoogleLogin onSuccess={handleSuccess} onError={() => console.log("Login Failed")} />
       </div>
       <p className="click-here-text">Click here to login</p>
     </div>
   );
 };
-
 
 export default Login;
