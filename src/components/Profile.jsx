6a0fa5efc534419c3fafddfbb160b6ab100c9693
123456NI-GIT <157794860+123456NI-GIT@users.noreply.github.com>
2025-04-08 15:10:12 +0530

    message

---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+// Profile.jsx
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="profile-page">
+      <h1>This is the Profile Page</h1>
+      {/* You can add more profile info here */}
+    </div>
+  );
+};
+
+export default Profile;
