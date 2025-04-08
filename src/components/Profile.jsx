0256a49b77b1d2639145e2295d28bef25ea9e8b5
123456NI-GIT <157794860+123456NI-GIT@users.noreply.github.com>
2025-04-08 15:12:09 +0530

    message

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,18 @@
 // Profile.jsx
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1); // This will take the user to the previous page
+  };
+
   return (
     <div className="profile-page">
       <h1>This is the Profile Page</h1>
-      {/* You can add more profile info here */}
+      <button onClick={goBack}>Go Back</button>
     </div>
   );
 };
